92a7f11056fe995c61e593878a0d55ef50bccd24
Mario Araya <marioaxtrem@gmail.com>
2022-10-11 17:35:55 -0600

    modificacion de scripts para ingresarlos por consola en mysql

--- dataBase/scriptCompleto.sql
+++ dataBase/scriptCompleto.sql
@@ -31,30 +31,42 @@ BEGIN
 SELECT * FROM images;
 END&&
 
-CREATE OR ALTER PROCEDURE updateImage @imageName varchar(40), @image text 
-AS BEGIN
-update images set image = @image, dateAdded = CURRENT_TIMESTAMP where imageName = @imageName;
-END;
-GO
+DELIMITER &&
+CREATE PROCEDURE updateImage (in image_Name varchar(40), in image_data text)
+BEGIN
+update images set image = image_data, dateAdded = CURRENT_TIMESTAMP where imageName = image_Name;
+END&&
 
-CREATE OR ALTER PROCEDURE deleteImage @imageName varchar(40)
-AS BEGIN
-delete from images where imageName = @imageName;
-delete from results where imageName = @imageName;
-END;
-GO
+DELIMITER &&
+CREATE PROCEDURE deleteImage (in image_Name varchar(40))
+BEGIN
+delete from images where imageName = image_Name;
+delete from results where imageName = image_Name;
+END&&
 
-CREATE OR ALTER PROCEDURE addResults @imageName varchar(40), @results varchar(100) 
-AS BEGIN
-insert into results (imageName,results,dateModified) values (@imageName, @results, CURRENT_TIMESTAMP);
-END;
-GO
+DELIMITER &&
+CREATE PROCEDURE addResults (in image_Name varchar(40), in image_results varchar(100))
+BEGIN
+insert into results (imageName,results,dateModified) values (image_Name, image_results, CURRENT_TIMESTAMP);
+END&&
 
-CREATE OR ALTER PROCEDURE updateResults @imageName varchar(40), @results varchar(100) 
-AS BEGIN
-update results set results = @results, dateModified = CURRENT_TIMESTAMP where imageName = @imageName;
-END;
-GO
+DELIMITER &&
+CREATE PROCEDURE viewResults()
+BEGIN
+select * from results;
+END&&
+
+DELIMITER &&
+CREATE PROCEDURE viewImageResults(in image_Name varchar(40))
+BEGIN
+select results.results from results where imageName = image_Name;
+END&&
+
+DELIMITER &&
+CREATE PROCEDURE updateResults (in image_Name varchar(40), in image_results varchar(100)) 
+BEGIN
+update results set results = image_results, dateModified = CURRENT_TIMESTAMP where imageName = image_Name;
+END&&
 
 /*
 select * from images;
