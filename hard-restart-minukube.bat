2fa71017fd5bb69423d98617d75cf147ad679cbf
Fabian Ramirez <ramirezfabian003@gmail.com>
2022-10-20 03:01:10 -0600

    final

--- hard-restart-minukube.bat
+++ hard-restart-minukube.bat
@@ -13,3 +13,7 @@ kubectl apply -n rabbits -f Broker/Files
 ::timeout 60
 
 :: kubectl -n rabbits port-forward service/frontend-service 80:90
+
+:: kubectl -n rabbits port-forward service/mysql 3306:3306
+
+:: kubectl -n rabbits port-forward rabbitmq-0 15672:15672
